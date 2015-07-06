6a1ea303fd2c986b625211e8dfcab4e5b756059c
tecgen <>
2015-07-06 14:43:57 +0200

    new class to handle JIRA ticket creation

---
+++ src/main/java/com/github/gastaldi/jira/CreateNewTicketMojo.java
@@ -0,0 +1,12 @@
+package com.github.gastaldi.jira;
+
+/**
+ * This class creates a JIRA ticket within the JIRA version and prints out the name e.g. APP-1234. The ticket
+ * name can then be used for the next commit message.
+ *
+ * @author Marco Pehla
+ */
+public class CreateNewTicketMojo {
+
+    //TODO
+}
